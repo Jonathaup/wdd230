676047ef080f08d3873f90988bd23eb89c6b7833
Jonathaup <jonathanjacoburibepinto@gmail.com>
2022-04-23 22:22:41 -0600

    Homework

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Jonathan Uribe';
+
+const myHeading2 = document.querySelector('h2');
+myHeading2.textContent = 'WDD 230: Assignment Portal';
+
+const myday = document.querySelector('p');
+myday.textContent =  Date() ;
+
+const modifique = document.querySelector('.papa')
+modifique.textContent = ("Last Modification: " + document.lastModified);
