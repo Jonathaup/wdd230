10a75e2746bad85a3f8e2443e06f83297b9da4f7
Jonathaup <103954802+Jonathaup@users.noreply.github.com>
2022-05-01 00:24:07 -0600

    Update script.js

--- lesson2/js/script.js
+++ lesson2/js/script.js
@@ -1,22 +1,5 @@
-window.addEventListener("load", (event)=>{
-
-    const cry = document.querySelector('#copyrightyear');
-    cry.textContent = new Date().getFullYear();
-
-    const lu = document.querySelector('#lastupdated');
-    lu.textContent = document.lastModified;
-
-})
-
-madelink = () =>{
-    window.open('https://www.made.com/');   
-    
-}
-
-amazonlink = () =>{
-    window.open('https://www.amazon.com/s?k=bowl&ref=nb_sb_noss_1')
-}
-
-wixlink= () =>{
-    window.open('https://www.wix.com/');
-}
+let date = new Date();
+let year = date.getFullYear();
+document.getElementById("year").textContent = year;
+let modified = document.lastModified;
+document.getElementById("modified").textContent = modified;
