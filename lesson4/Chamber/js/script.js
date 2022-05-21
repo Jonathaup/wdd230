b73a7f770f9f2d4ca603b01bf1a50d467f797bec
Jonathaup <jonathanjacoburibepinto@gmail.com>
2022-05-21 12:56:10 -0600

    Homework

--- lesson4/Chamber/js/script.js
+++ lesson4/Chamber/js/script.js
@@ -1,4 +1,5 @@
-const currentDateSpan = document.querySelector("#Date");
+/* Get Current Date for Header */
+const currentDateSpan = document.querySelector("#currentDate");
 
 const now = new Date();
 currentDateSpan.textContent = new Intl.DateTimeFormat("en-GB", {
@@ -8,8 +9,13 @@ currentDateSpan.textContent = new Intl.DateTimeFormat("en-GB", {
 
 /* hamburger toggle menu */
 function toggleMenu() {
-  document.getElementsByClassName("navigate")[0].classList.toggle("respond");
+    document.getElementById("mainNav").classList.toggle("open");
+    document.getElementById("menuButton").classList.toggle("open");
 }
+
+const x = document.getElementById('menuButton');
+
+x.onclick = toggleMenu;
 
 /* last modified */
 let nd = new Date();
@@ -17,3 +23,15 @@ let nd = new Date();
 document.getElementById("currentYear").textContent = nd.getFullYear();
 
 document.querySelector("#modDate").textContent = document.lastModified;
+
+
+/* Banner on Mondays & Tuesdays */
+if(now.getDay() === 2) {
+    document.getElementById("banner").style.display = "block";
+}
+
+const close = document.querySelector("#close");
+
+close.addEventListener("click", () => {
+    banner.style.display = "none";
+});
