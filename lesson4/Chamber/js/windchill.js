b62f824de196b5634d9137091a9eeab6d74f99a5
Jonathaup <Jonathanjacoburibepinto@gmail.com>
2022-06-20 10:04:50 -0600

    Homework

--- lesson4/Chamber/js/windchill.js
+++ lesson4/Chamber/js/windchill.js
@@ -1,14 +1,33 @@
-const temp = parseFloat(document.querySelector("#temp").innerHTML);
-const windspeed = parseFloat(document.querySelector("#windspeed").innerHTML);
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
 
-const windchill = document.querySelector("#windchill");
-if ((temp <= 50) && (windspeed > 3)) {
-  const chill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed, .16)) + (0.4275 * temp * Math.pow(windspeed, .16));
-  windchill.innerHTML = chill.toFixed(1);
-} else {
-  windchill.innerHTML = "N/A";
-}
+const url =
+  "//api.openweathermap.org/data/2.5/weather?q=HollyHill&units=imperial&appid=cbdbf1656275e48900a3931f13929b10";
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    //console.log(data);//
 
+    const windspeed = document.querySelector("#windspeed");
 
+    currentTemp.innerHTML = `${data.main.temp.toFixed(0)}`;
+    windspeed.innerHTML = `${data.wind.speed}`;
 
+    const windchill = document.querySelector("#windchill");
+    if ((currentTemp <= 50) && (windspeed > 3)) {
+    const chill = 35.74 + (0.6215 * currentTemp) - (35.75 * Math.pow(windspeed, .16)) + (0.4275 * currentTemp * Math.pow(windspeed, .16));
+    windchill.innerHTML = chill.toFixed(1);
+    } else {
+    windchill.innerHTML = "N/A";
+    }
 
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const desc = data.weather[0].description;
+
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", desc);
+    captionDesc.innerHTML = desc;
+
+});
+   
