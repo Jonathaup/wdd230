9055e7c9d4619210e76394dedd02e301a6b39f48
Jonathaup <Jonathanjacoburibepinto@gmail.com>
2022-07-19 16:33:04 -0600

    Homework

---
+++ lesson13 Final-Project/js/spotlight.js
@@ -0,0 +1,89 @@
+const requestURL = "https://kellyo-sm.github.io/wdd230/templeinn/json/temples.json";
+const spotlt = document.querySelector("#spotlight");
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const temple = jsonObject["temples"];
+    shuffleArray(temple);
+    temple.forEach(displayTemple);
+  });
+
+function displayTemple(temple) {
+  
+    let card = document.createElement("article");
+    let name = document.createElement("h2");
+    let img = document.createElement("img");
+    let footer = document.createElement("div");
+    let address = document.createElement("h4");
+    let city = document.createElement("h4");
+    let country = document.createElement("h4");
+    let serviceslabel = document.createElement("h4");
+    let services = document.createElement("p");
+    let services2 = document.createElement("p");
+  
+
+    console.log(temple);
+
+    img.setAttribute('src', temple.img);
+    img.setAttribute('alt', `${temple.templeName}`);
+    img.setAttribute('loading', 'lazy');
+    img.classList.add("img");
+
+    name.textContent = temple.templeName;
+    address.textContent = temple.address;
+    city.textContent = temple.city;
+    country.textContent = temple.country;
+    serviceslabel.textContent = temple.label4;
+    services.textContent = temple.services;
+    services2.textContent = temple.services2;
+    
+
+    footer.appendChild(address);
+    footer.appendChild(city);
+    footer.appendChild(country);
+    card.appendChild(name);
+    card.appendChild(img);
+    card.appendChild(footer);
+    card.appendChild(serviceslabel);
+    card.appendChild(services);
+    card.appendChild(services2);
+    
+
+    document.querySelector("div#spotlight").appendChild(card);
+  }
+
+
+// function shuffleArray(array) {
+//   for (var i = array.length - 1; i > 0; i--) {
+//     var j = Math.floor(Math.random() * (i + 1));
+//     var temp = array[i];
+//     array[i] = array[j];
+//     array[j] = temp;
+//   }
+// }
+
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * 9);
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+// function shuffleArray(array) {
+//   let spotLight = array.length;
+//   while (0 !== spotLight) {
+//     let randId = Math.floor(Math.random() * spotLight);
+//     spotLight -= 1;
+//     let tmp = array[spotLight];
+//     array[spotLight] = array[randId];
+//     array[randId] = tmp;
+//   }
+//   return array;
+// }
---
+++ lesson13 Final-Project/js/temples.js
@@ -0,0 +1,162 @@
+const requestURL = "https://kellyo-sm.github.io/wdd230/templeinn/json/temples.json";
+const groups = document.querySelector(".cards");
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); 
+    const temples = jsonObject['temples'];
+    temples.forEach(displayTemples); 
+  });  
+
+function displayTemples(temple) {
+  // Create elements to add to the document 
+  let card = document.createElement('article');
+  let img = document.createElement('img');        
+  let templeName = document.createElement('h2');
+  let label1 = document.createElement('h3');
+  let address = document.createElement('p');
+  let city = document.createElement('p');
+  let country = document.createElement('p');
+  let label2 = document.createElement('h3');
+  let phone = document.createElement('p');
+  let label3 = document.createElement('h3');
+  let email = document.createElement('p');
+  let label4 = document.createElement('h3');
+  let services = document.createElement('p');
+  let label5 = document.createElement('h3');
+  let announcement = document.createElement('p');
+  let label6 = document.createElement('h3');
+  let groundbreaking = document.createElement('p');
+  let label7 = document.createElement('h3');
+  let dedication = document.createElement('p');
+  let ordinance = document.createElement('h3');
+  let ordschedule = document.createElement('p');
+  let session = document.createElement('h3');
+  let sesschedule = document.createElement('p');
+  let closures = document.createElement('h3');
+  let cl1 = document.createElement('p');
+  let cl2 = document.createElement('p');
+  let cl3 = document.createElement('p');
+  let cl4 = document.createElement('p');
+  let cl5 = document.createElement('p');
+  let cl6 = document.createElement('p');
+  let cl7 = document.createElement('p');
+  let cl8 = document.createElement('p');
+  let cl9 = document.createElement('p');
+  
+  let like = document.createElement('button');
+  // const likes = document.createElement('span');
+  // let num_likes = Number(window.localStorage.getItem(temple.letter));
+  
+
+  console.log(temple);
+
+
+  // Build the image attributes by using the setAttribute mefor the src, alt, and loading attribute values. (Fill inblank with the appropriate variable).
+  img.setAttribute('src', temple.img);
+  img.setAttribute('alt', `${temple.templeName}`);
+  img.setAttribute('loading', 'lazy');
+  img.classList.add('img');
+
+  
+  like.setAttribute('src', temple.like);
+  
+  // like.setAttribute('type', 'button');
+  // like.setAttribute('class', 'like');
+  // like.setAttribute('id', temple.letter);
+
+  // templeName.innerHTML = temple.templeName;
+  // address .innerHTML = `Address: ${temple.address}`;
+  // city.innerHTML = `Location: ${temple.city}`;
+  // phone.innerHTML = `Phone: ${temple.phone}`;
+  // email.innerHTML = `Email: ${temple.email}`;
+  // services.innerHTML = `Services: ${temple.services}`;
+  // closures.innerHTML = `Closing Schedule: ${temple.clschedule}`;
+  // likes.innerHTML = ' ' + num_likes + ' likes';
+
+          
+  // Change the textContent property of the h3 element to conthe companies full name
+  // templeName.textContent = `${temple.templeName}`;
+  templeName.textContent = temple.templeName;
+  // templeName.classList.add("tName");
+  label1.textContent = temple.label1;
+  // label1.classList.add("label1");
+  address.textContent = `${temple.address}`;
+  city.textContent = `${temple.city}`;
+  country.textContent = `${temple.country}`;
+  label2.textContent = `${temple.label2}`;
+  phone.textContent = `${temple.phone}`;
+  label3.textContent = `${temple.label3}`;
+  email.textContent = `${temple.email}`;
+  label4.textContent = `${temple.label4}`;
+  services.textContent = `${temple.services}`;
+  label5.textContent = `${temple.label5}`;
+  announcement.textContent = `${temple.announcements}`;
+  label6.textContent = `${temple.label6}`;
+  groundbreaking.textContent = `${temple.grndbrkDate}`;
+  label7.textContent = `${temple.label7}`;
+  dedication.textContent = `${temple.dedication}`;
+  session.textContent = `${temple.session}`;
+  sesschedule.textContent = `${temple.sesschedule}`;
+  ordinance.textContent = `${temple.ordinance}`;
+  ordschedule.textContent = `${temple.ordSchedule}`;
+  closures.textContent = `${temple.clschedule}`;
+  cl1.textContent = `${temple.cl1}`;
+  cl2.textContent = `${temple.cl2}`;
+  cl3.textContent = `${temple.cl3}`;
+  cl4.textContent = `${temple.cl4}`;
+  cl5.textContent = `${temple.cl5}`;
+  cl6.textContent = `${temple.cl6}`;
+  cl7.textContent = `${temple.cl7}`;
+  cl8.textContent = `${temple.cl8}`;
+  cl9.textContent = `${temple.cl9}`;
+  
+  
+          
+  // Add/append the section(card) with the h2 element
+  card.appendChild(img);
+  card.appendChild(templeName);
+  card.appendChild(label1);
+  card.appendChild(address);
+  card.appendChild(label2);
+  card.appendChild(phone);
+  card.appendChild(label3);
+  card.appendChild(email);
+  card.appendChild(label4);
+  card.appendChild(services);
+  card.appendChild(label5);
+  card.appendChild(announcement);
+  card.appendChild(label6);
+  card.appendChild(groundbreaking);
+  card.appendChild(label7);
+  card.appendChild(dedication);
+  card.appendChild(session);
+  card.appendChild(sesschedule);
+  card.appendChild(ordinance);
+  card.appendChild(ordschedule);
+  card.appendChild(closures);
+  card.appendChild(cl1);
+  card.appendChild(cl2);
+  card.appendChild(cl3);
+
+  card.appendChild(like);
+  
+            
+          
+  // Add/append the existing HTML div with the cards class the section(card)
+  //document.querySelector('div.cards').appendChild(card);
+  // groups.append(group);
+  document.querySelector("div.cards").appendChild(card);
+
+  // function Like() {
+  //   num_likes++;
+  //   localStorage.setItem(like.id, num_likes);
+  //   window.location.reload();
+  // }
+
+  // like.onclick = Like;
+          
+};
